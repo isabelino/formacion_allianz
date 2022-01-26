692be9efc54521493ca507ef1996d9f2161e16fa
rolando <isa.klopez@hotmail.com>
2022-01-26 13:44:29 +0100

    actualizacion de codigo

--- hellospring/src/main/java/com/formacion/spring/hellospring/HellospringApplication.java
+++ hellospring/src/main/java/com/formacion/spring/hellospring/HellospringApplication.java
@@ -8,6 +8,8 @@ public class HellospringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellospringApplication.class, args);
+		
+		
 	}
 
 }
---
+++ hellospring/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Jose','Perez','jp@email.com',631224558,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Carlos','Lopez','cl@email.com',92124445,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Maria','Orellana','mo@email.com',8954545,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Dina','Ramirez','dr@email.com',63154545,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Pepe','Casillas','pc@email.com',654544558,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Enrrique','Iglesias','ei@email.com',54578558,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Pedro','Diaz','pd@email.com',91245454,'2022-01-26');
+INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Ramon','Gonzalez','rg@email.com',25454558,'2022-01-26');
