838dfb5a9b636e46866379b482e512d236f2a13b
rolando <isa.klopez@hotmail.com>
2022-01-31 11:53:39 +0100

    ultimos cambios

--- hellospring/src/main/resources/import.sql
+++ hellospring/src/main/resources/import.sql
@@ -6,3 +6,15 @@ INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Pepe','
 INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Enrrique','Iglesias','ei@email.com',54578558,'2022-01-26');
 INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Pedro','Diaz','pd@email.com',91245454,'2022-01-26');
 INSERT INTO clientes (nombre,apellido,email,telefono,create_at) VALUES ('Ramon','Gonzalez','rg@email.com',25454558,'2022-01-26');
+
+
+
+INSERT INTO usuarios (username,password,enabled) VALUES('rolando','1234',1);
+INSERT INTO usuarios (username,password,enabled) VALUES('admin','1234',1);
+
+INSERT INTO roles (nombre) VALUES('ROLE_USER');
+INSERT INTO roles (nombre) VALUES('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(1,1);
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(2,2);
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(2,1);
