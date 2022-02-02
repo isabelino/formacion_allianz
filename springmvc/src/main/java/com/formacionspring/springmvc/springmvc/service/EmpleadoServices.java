20d2ef72c740ba35b6e39bddbdda5355f22ed310
rolando <isa.klopez@hotmail.com>
2022-02-02 13:53:28 +0100

    ultimos cambios

--- springmvc/src/main/java/com/formacionspring/springmvc/springmvc/service/EmpleadoServices.java
+++ springmvc/src/main/java/com/formacionspring/springmvc/springmvc/service/EmpleadoServices.java
@@ -7,4 +7,10 @@ import com.formacionspring.springmvc.springmvc.entity.Empleado;
 public interface EmpleadoServices {
 
 	public List<Empleado> listarTodosLosEmpleados();
+	
+	public Empleado guardarEmpleado(Empleado empleado);
+	
+	public Empleado obtenerEmpleadoPorId(Long id);
+	
+	public void eliminarEmpleado(Long id);
 }
